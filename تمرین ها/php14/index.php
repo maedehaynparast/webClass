432e09a1f0ae46e2cb864bb0065a1f33424d7404
maedehaynparast <159957292+maedehaynparast@users.noreply.github.com>
2024-04-16 20:46:33 -0700

    add file

---
+++ تمرین ها/php14/index.php
@@ -0,0 +1,106 @@
+<?php
+
+
+ //string 
+ $name="maedeh";
+ $job='bisines';
+ $summary="i'm maedeh";
+
+ echo  $name;
+ echo "</br>";
+ echo $job;
+ echo "</br>";
+ echo $summary;
+ echo "</br>";
+
+ //integer
+ $age=19;
+ echo $age;
+ echo "</br>";
+
+ //float
+ $weight=23.9;
+ echo $weight;
+ echo "</br>";
+
+ //boolean
+ $woman=true;
+ echo true;
+ echo "</br>";
+ 
+ //operator
+ $str='17';
+ $int=15;
+ $sum=$str+$int;
+ echo ($sum);
+ echo "</br>";
+ 
+ //مثال برای مساحبه ریاضی
+ $x=18;
+ $y=9;
+ 
+ echo $x+$y;
+ echo "</br>";
+
+ echo $x-$y;
+ echo "</br>";
+
+ echo $x*$y;
+ echo "</br>";
+
+ echo $x%$y;
+ echo "</br>";
+
+ echo $x/$y;
+ echo "</br>";
+
+ $x=35;
+ $x+=65;
+ echo $x;
+ echo "</br>";
+
+ $x=50;
+ $x=50+20;
+ echo $x;
+ echo "</br>";
+
+ $x=18;
+ $y=9;
+
+ echo '$x==$y=>';
+ echo $x==$y;
+ echo "</br>";
+
+ echo '$x===$y=>';
+ echo $x===$y;
+ echo "</br>";
+
+ echo '$x!=$y=>';
+ echo $x!=$y;
+ echo "</br>";
+
+ echo '$x<>$y=>';
+ echo $x<>$y;
+ echo "</br>";
+
+ echo '$x!==$y=>';
+ echo $x!==$y;
+ echo "</br>";
+
+ echo '$x>$y=>';
+ echo $x>$y;
+ echo "</br>";
+
+ echo '$x<$y=>';
+ echo $x<$y;
+ echo "</br>";
+
+ echo '$x>=$y=>';
+ echo $x>=$y;
+ echo "</br>";
+
+ echo '$x<=$y=>';
+ echo $x<=$y;
+ echo "</br>";
+
+?>
