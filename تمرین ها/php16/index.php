e802898568588eab23613cbed02e5d953e80ccd3
maedehaynparast <159957292+maedehaynparast@users.noreply.github.com>
2024-04-20 04:32:46 -0700

    add file

---
+++ تمرین ها/php16/index.php
@@ -0,0 +1,26 @@
+<?php
+$name=array(0,30);
+// 0-10=sang 0
+//   10-20=kaghaz 1
+//      20-30=ghechi 2
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>sang,kaghaz,ghechi</title>
+</head>
+<body>
+    <label> hads bezanin </label>
+    </br>
+    <select name="dade1">
+        <option value="0">sang</option>
+        <option value="1">kaghaz</option>
+        <option value="2">ghechi</option>
+    </select>
+    <input type="submit">
+</body>
+</html>
